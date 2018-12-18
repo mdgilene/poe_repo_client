5c1f52553795fdf53c35dfbdbfbe83ea694be4e0
Gilene <Matt.Gilene@us.sogeti.com>
2018-12-18 13:14:37 -0500

    Initial Build Page

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "@/views/Home.vue";
+import Build from "@/views/Build.vue";
 
 Vue.use(Router);
 
@@ -12,6 +13,11 @@ export default new Router({
       path: "/",
       name: "home",
       component: Home
+    },
+    {
+      path: "/view-build/:id",
+      name: "build",
+      component: Build
     }
   ]
 });
---
+++ src/views/Build.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>{{ build.name }}</h1>
+    <h3>Author: {{ build.author.username }}</h3>
+
+    <h2>Introduction</h2>
+    <p>{{ build.introduction_text }}</p>
+
+    <h2>Cost</h2>
+    <p>{{ build.cost_text }}</p>
+
+    <h2>Items</h2>
+    <p>{{ build.item_text }}</p>
+    <div v-for="item in build.items" :key="item.id">{{ item }}</div>
+
+    <h2>Gems</h2>
+    <p>{{ build.gem_text || "This doesn't exist yet in the build model" }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      build: {}
+    };
+  },
+  async mounted() {
+    const { data } = await this.$http.get(
+      `http://localhost:5000/api/builds/${this.$route.params.id}`
+    );
+    this.build = data;
+  }
+};
+</script>
+
+<style></style>
